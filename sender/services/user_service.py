8ca126c71524d47646c3c3deb1878bf992ca069a
Кулагин Андрей Андреевич <s121917@guu.ru>
2023-07-13 14:00:21 +0300

    normalize_errors and fix some bugs

--- sender/services/user_service.py
+++ sender/services/user_service.py
@@ -7,23 +7,21 @@ from rest_framework.request import Request
 
 
 def user_data_validate(data: collections.OrderedDict, request: Request) -> collections.OrderedDict:
-    username_is_already_occupied_error = "Имя пользователя занято"
     # проверяем, есть ли уже пользователи с таким ником
-    if "username" in data:
-        if len(User.objects.filter(username=data["username"])) != 0:
-            raise serializers.ValidationError(username_is_already_occupied_error)
     if request.method == "POST":
         if User.objects.filter(email=data['email']).exists():
-            raise serializers.ValidationError("User with this email already exists.")
+            raise serializers.ValidationError("Пользователь с таким Email уже существует")
     if 'password' in data:
         if 'confirm_password' in data:
             if data['password'] != data['confirm_password']:
-                raise serializers.ValidationError("The two password fields must match.")
+                raise serializers.ValidationError("Пароль и подтверждение пароля не совпадают")
         else:
             raise serializers.ValidationError("Подтвердите пароль")
     if 'username' in data:
         if User.objects.filter(username=data['username']).exists():
             raise serializers.ValidationError("Пользователь с таким именем уже существует")
+        if len(data["username"]) == 0:
+            raise serializers.ValidationError("Имя пользователя не может быть пустым")
     if 'confirm_password' in data:
         data.pop('confirm_password')
     return data
@@ -40,7 +38,12 @@ def user_create(serializer: serializers.ModelSerializer) -> bool:
     validated_data = serializer.validated_data
     is_password_random = False
     if not ("username" in validated_data):
-        validated_data['username'] = validated_data['email'].split("@")[0]
+        auto_username = validated_data['email'].split("@")[0]
+        try:
+            User.objects.get(username=auto_username)
+            validated_data['username'] = validated_data['email']
+        except User.DoesNotExist:
+            validated_data['username'] = auto_username
     if 'password' in validated_data:
         password = validated_data.pop('password')
     else:
